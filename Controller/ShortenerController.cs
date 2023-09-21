1855720471f7d46016c43f62fe56cb4f41f64efc
saulogp <sauloproetti@gmail.com>
2023-09-21 20:19:10 -0300

    segregando classes

--- Controller/ShortenerController.cs
+++ Controller/ShortenerController.cs
@@ -1,4 +1,4 @@
-using System.Text;
+using Core;
 using Infrastructure.Cache;
 using Microsoft.AspNetCore.Mvc;
 using Model.Request;
@@ -7,42 +7,65 @@ using Model.Response;
 namespace Controller;
 public static class ShortenerController
 {
-    public static void ConfigureShortenerController(this WebApplication app){
+    public static void ConfigureShortenerController(this WebApplication app)
+    {
         app.MapGet("{code}", RedirectShortLink);
         app.MapGet("api/shortener/{code}", GetShortLink);
         app.MapPost("api/shortener", CreateShortLink);
     }
-    
-    private static async Task<IResult> RedirectShortLink(string code, ICachingService _cache){
-        var originalUrl = await _cache.GetAsync(code);
-        return Results.Redirect(originalUrl);
-    }
-    
-    private static async Task<IResult> GetShortLink(string code, ICachingService _cache){
-        var originalUrl = await _cache.GetAsync(code);
-        return Results.Ok(new GetShortLinkResponse(code,originalUrl));
-    }
 
-    private static async Task<IResult> CreateShortLink([FromBody] CreateShortLinkRequest request, ICachingService _cache){
-        var randomKey = GenerateRandomCode(5);
-        randomKey+=request.Alias;
-        await _cache.SetAsync(randomKey,request.OriginalUrl);
-        return Results.Created($"api/shortener/{randomKey}", new CreateShortlinkResponse(randomKey));
-    }
-
-    static Random random = new Random();
-    static string GenerateRandomCode(int length)
+    private static async Task<IResult> RedirectShortLink(string code, ICachingService _cache)
     {
-        const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        StringBuilder code = new StringBuilder();
-        
-        for (int i = 0; i < length; i++)
+        try
         {
-            int index = random.Next(characters.Length);
-            code.Append(characters[index]);
+            var originalUrl = await _cache.GetAsync(code.ToUpper());
+            
+            if(string.IsNullOrEmpty(originalUrl)) 
+                return Results.BadRequest("Link não encontrado");
+            
+            return Results.Redirect(originalUrl);
         }
-        
-        return code.ToString();
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ocorreu uma exceção ao obter o valor do cache: {ex.Message}");
+            return Results.BadRequest("Ocorreu um erro ao redirecionar o link.");
+        }
     }
 
+    private static async Task<IResult> GetShortLink(string code, ICachingService _cache)
+    {
+        try
+        {
+            var originalUrl = await _cache.GetAsync(code.ToUpper());
+            
+            if(string.IsNullOrEmpty(originalUrl)) 
+                return Results.BadRequest("Link não encontrado");
+
+            return Results.Ok(new GetShortLinkResponse(code.ToUpper(), originalUrl));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ocorreu uma exceção ao obter o valor do cache: {ex.Message}");
+            return Results.BadRequest("Ocorreu um erro ao recuperar o link.");
+        }
+    }
+
+    private static async Task<IResult> CreateShortLink([FromBody] CreateShortLinkRequest request, ICachingService _cache)
+    {
+        try
+        {
+            var randomKey = ShortenerExtensions.GenerateRandomCode(5);
+            randomKey =  string.IsNullOrEmpty(request.Alias) 
+                ? randomKey 
+                : $"{request.Alias.ToUpper()}-{randomKey}";
+
+            await _cache.SetAsync(randomKey, request.OriginalUrl);
+            return Results.Created($"api/shortener/{randomKey}", new CreateShortlinkResponse(randomKey));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ocorreu uma exceção ao gravar valor no cache: {ex.Message}");
+            return Results.BadRequest("Ocorreu um erro ao criar o link encurtado.");
+        }
+    }
 }
---
+++ Core/ShortenerExtensions.cs
@@ -0,0 +1,20 @@
+using System.Text;
+
+namespace Core;
+public static class ShortenerExtensions
+{
+    static Random random = new Random();
+    public static string GenerateRandomCode(int length)
+    {
+        const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder code = new();
+        
+        for (int i = 0; i < length; i++)
+        {
+            int index = random.Next(characters.Length);
+            code.Append(characters[index]);
+        }
+        
+        return code.ToString();
+    }
+}
